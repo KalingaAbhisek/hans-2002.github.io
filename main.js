61d322064099b9ae8051ff5d806c5ac81eb04c5a
Hans Pattnaik <64223974+HANS-2002@users.noreply.github.com>
2021-02-26 19:36:43 +0530

    Major Update
    Duplicate Google Update

---
+++ main.js
@@ -0,0 +1,25 @@
+function search()
+{
+    let searchId = document.getElementById("search").value;
+    let site = 'https://www.google.com/search?q='+searchId;
+    window.open(site,'_blank');
+}
+
+function startDictation() 
+{
+    if (window.hasOwnProperty('webkitSpeechRecognition')) 
+    {
+        var recognition = new webkitSpeechRecognition();
+        continuous = false;
+        interimResults = false;
+        lang = "en-US";
+        start();
+        onresult = function(e) {
+            getElementById('search').value = e.results[0][0].transcript;
+            stop();
+        };
+        onerror = function(e) {
+            stop();
+        };
+    }
+}
